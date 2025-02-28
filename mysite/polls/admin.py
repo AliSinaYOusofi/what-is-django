193313ea970a827e1f201f2bfd81f04c755b023e
HajiAli <senayousofiali@gmail.com>
2025-02-28 20:40:12 +0430

    added: familiar with the basics

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,9 @@ from .models import Choice
 # Register your models here.
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 
-admin.site.register(Questions)
-admin.site.register(Choice)
+# admin.site.register(Questions)
+admin.site.register(Choice) 
+admin.site.register(Questions, QuestionAdmin)
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,6 +5,11 @@ from polls.models import Questions
 
 class QuestionModelTest(TestCase):
     def test_was_published_recently_with_future_questions(self):
-        time = timezone.now() + datetime.timedelta(days=30)
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
         future_question = Questions(pub_date=time)
         self.assertIs(future_question.was_published_recently, False, "Future is not recent")
+        
+    def test_was_published_recentyl_with_recent_question(slef):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
